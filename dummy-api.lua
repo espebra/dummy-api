91bd9bb47e596d6c06aa5152aa29fbfb0355106c
Espen Braastad <espebra@ifi.uio.no>
2015-05-03 09:09:11 +0200

    License

--- dummy-api.lua
+++ dummy-api.lua
@@ -1,3 +1,29 @@
+--[[
+Copyright (c) 2015, Espen Braastad
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--]]
+
 local ngx = require "ngx"
 local cjson = require "cjson"
 local random = require "random"
@@ -14,15 +40,6 @@ function get_property(key)
         end
     end
     return nil
-end
-
-function generate_string(l)
-    if l < 1 then return nil end -- Check for l < 1
-    local s = "" -- Start string
-    for i = 1, l do
-        s = s .. string.char(math.random(32, 126)) -- Generate random number from 32 to 126, turn it into character and add to string
-    end
-    return s -- Return string
 end
 
 -- Setting defaults
